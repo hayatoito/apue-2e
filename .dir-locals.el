85280f6f922d62753256ed78ae430860e9d338cd
Hayato Ito <hayato@chromium.org>
2010-04-17 20:34:51 +0900

    Add flymake varialbes

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,6 @@
 ;;; .dir-locals.el ---
-((c-mode . ((c-basic-offset . 4))))
+((c-mode . ((c-basic-offset . 4)
+            (google-flymake-do-not-pass-filename-to-syntax-checker . t)
+            (google-flymake-local-command . "waf")
+            (my-flymake-enable-p . t)
+            )))
